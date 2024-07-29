d2f6d2d8867a4723900928eeed555a0a0ec5695e
Carlos Eduardo <ducameneses16@gmail.com>
2024-07-29 18:02:54 -0300

    feat: Create the models and database settings

---
+++ madr/database.py
@@ -0,0 +1,13 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from madr.settings import Settings
+
+engine = create_async_engine(Settings().DATABASE_URL)
+
+
+async def get_async_engine() -> AsyncGenerator:
+    async with AsyncSession(engine) as session:
+        yield session
