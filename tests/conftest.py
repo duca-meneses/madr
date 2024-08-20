f30c9ce2905ad435c5d9404067c83a9bcbc688e8
Carlos Eduardo <ducameneses16@gmail.com>
2024-08-20 01:12:26 -0300

    test: Modify tests for adapt with asyncio

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,70 @@
 import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from factory import Factory, LazyAttribute, Sequence
+from faker.generator import Generator
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    create_async_engine,
+)
+from testcontainers.postgres import PostgresContainer
 
 from madr.app import app
-from madr.models import table_registry
+from madr.database import get_async_session
+from madr.models import Account, table_registry
+
+
+class AccountFactory(Factory):
+    class Meta:
+        model = Account
+
+    username = Sequence(lambda x: f'test{x}')
+    email = LazyAttribute(lambda obj: f'{obj.username}@test.com')
+    password = LazyAttribute(lambda obj: f'{obj.username}@10')
+
+
+@pytest.fixture(scope='session')
+def engine():
+    with PostgresContainer('postgres:16', driver='psycopg') as postgres:
+        # postgres.driver = 'asyncpg'
+        yield create_async_engine(postgres.get_connection_url())
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+async def session(engine: AsyncEngine):
+    async with engine.connect() as connection:
+        await connection.run_sync(
+            lambda con: table_registry.metadata.create_all(con.engine)
+        )
+
+        async with AsyncSession(engine, expire_on_commit=False) as session:
+            yield session
+
+        await connection.run_sync(
+            lambda con: table_registry.metadata.drop_all(con.engine)
+        )
 
 
 @pytest.fixture
-def session():
-    engine = create_engine('sqlite:///:memory:')
-    table_registry.metadata.create_all(engine)
-
-    with Session(engine) as session:
+async def client(session: AsyncSession):
+    async def get_session_override():
         yield session
 
-    table_registry.metadata.drop_all(engine)
+    app.dependency_overrides[get_async_session] = get_session_override
+
+    async with AsyncClient(app=app, base_url='http://test') as test_client:
+        yield test_client
+
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture
+async def user(faker: Generator, session: AsyncSession) -> Account:
+    password = faker.password()
+    new_user = AccountFactory(password=password)
+
+    session.add(new_user)
+    await session.commit()
+    await session.refresh(new_user)
+
+    return new_user
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,16 +1,17 @@
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from madr.models import Account
 
 
-def test_create_account(session):
+async def test_create_account(session: AsyncSession):
     new_account = Account(
         username='duca_meneses', password='secret', email='duca@test.com'
     )
     session.add(new_account)
-    session.commit()
+    await session.commit()
 
-    account = session.scalar(
+    account = await session.scalar(
         select(Account).where(Account.username == new_account.username)
     )
 
