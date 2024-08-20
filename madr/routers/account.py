04e194da3f739fd75a0866782c6ab5f02254a2ae
Carlos Eduardo <ducameneses16@gmail.com>
2024-08-20 01:09:48 -0300

    chore: Implements a mini refact in codes

--- madr/routers/account.py
+++ madr/routers/account.py
@@ -19,8 +19,8 @@ router = APIRouter(prefix='/users', tags=['users'])
 async def create_account(account: AccountSchema, session: T_Session):
     db_account = await session.scalar(
         select(Account).where(
-            (Account.username == account.username)
-            or (Account.email == account.email)
+            (Account.username == account.username) |
+            (Account.email == account.email)
         )
     )
 
@@ -28,19 +28,18 @@ async def create_account(account: AccountSchema, session: T_Session):
         if db_account.username == account.username:
             raise HTTPException(
                 status_code=HTTPStatus.CONFLICT,
-                detail='Username already exists'
+                detail='Username already exists',
             )
         elif db_account.email == account.email:
             raise HTTPException(
-                status_code=HTTPStatus.CONFLICT,
-                detail='Email already exists'
+                status_code=HTTPStatus.CONFLICT, detail='Email already exists'
             )
 
     db_account = Account(
-            username=account.username,
-            email=account.email,
-            password=account.password
-        )
+        username=account.username,
+        email=account.email,
+        password=account.password,
+    )
 
     session.add(db_account)
     await session.commit()
@@ -56,9 +55,7 @@ async def list_accounts(session: T_Session, limit: int = 10, skip: int = 0):
 
 
 @router.get(
-    '/{user_id}',
-    response_model=AccountPublic,
-    status_code=HTTPStatus.OK
+    '/{user_id}', response_model=AccountPublic, status_code=HTTPStatus.OK
 )
 async def get_account_by_id(user_id: int, session: T_Session):
     account = await session.scalar(
@@ -67,29 +64,23 @@ async def get_account_by_id(user_id: int, session: T_Session):
 
     if not account:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail='User not found'
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
     return account
 
 
 @router.put(
-    '/{user_id}',
-    response_model=AccountPublic,
-    status_code=HTTPStatus.OK
+    '/{user_id}', response_model=AccountPublic, status_code=HTTPStatus.OK
 )
 async def update_account(
-    user_id: int,
-    account: AccountSchema,
-    session: T_Session
+    user_id: int, account: AccountSchema, session: T_Session
 ):
     account_db = await session.scalar(
         select(Account).where(Account.id == user_id)
     )
     if not account_db:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail='User not found'
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
 
     account_db.email = account.email
@@ -103,9 +94,7 @@ async def update_account(
 
 
 @router.delete(
-    '/user_id',
-    response_model=MessageSchema,
-    status_code=HTTPStatus.OK
+    '/{user_id}', response_model=MessageSchema, status_code=HTTPStatus.OK
 )
 async def delete_account(user_id: int, session: T_Session):
     account_db = await session.scalar(
@@ -114,8 +103,7 @@ async def delete_account(user_id: int, session: T_Session):
 
     if not account_db:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail='User not found'
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
 
     await session.delete(account_db)
