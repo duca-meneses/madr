7459f66cecad31547f640cf8c5e14a091ec40247
Carlos Eduardo <ducameneses16@gmail.com>
2024-08-21 00:37:19 -0300

    test: Create tests for the security part, auth routers and adapt the account router tests

---
+++ tests/routers/test_auth.py
@@ -0,0 +1,85 @@
+from http import HTTPStatus
+
+from freezegun import freeze_time
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from madr.models import Account
+
+
+async def test_get_token(client: AsyncClient, user: Account):
+    response = await client.post(
+        '/auth/token',
+        data={'username': user.email, 'password': user.clean_password}
+    )
+
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert token['token_type'] == 'Bearer'
+    assert 'access_token' in token
+
+
+async def test_login_for_access_token_incorrect_credentials(
+    client: AsyncClient, session: AsyncSession
+):
+    response = await client.post(
+        'auth/token',
+        data={'username': 'wrong@email.com', 'password': 'wrong_password'},
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+async def test_token_wrong_password(client: AsyncClient, user: Account):
+    response = await client.post(
+        'auth/token',
+        data={'username': user.email, 'password': 'wrong_password'},
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+async def test_token_wrong_email(client: AsyncClient, user: Account):
+    response = await client.post(
+        'auth/token',
+        data={'username': 'wrong@email.com', 'password': user.clean_password},
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+async def test_refresh_token(client: AsyncClient, token):
+    response = await client.post(
+        'auth/refresh_token',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    data = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'access_token' in data
+    assert 'token_type' in data
+    assert data['token_type'] == 'Bearer'
+
+
+async def test_token_expired_dont_refresh(client: AsyncClient, user: Account):
+    with freeze_time('2023-07-14 12:00:00'):
+        response = await client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password},
+        )
+
+        assert response.status_code == HTTPStatus.OK
+        token = response.json()['access_token']
+
+    with freeze_time('2023-07-14 12:31:00'):
+        response = await client.post(
+            '/auth/refresh_token', headers={'Authorization': f'Bearer {token}'}
+        )
+
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json() == {'detail': 'Could not validate credentials'}
---
+++ tests/test_security.py
@@ -0,0 +1,80 @@
+from http import HTTPStatus
+
+import pytest
+from fastapi import HTTPException
+from freezegun import freeze_time
+from httpx import AsyncClient
+from jwt import decode
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from madr.models import Account
+from madr.security import create_access_token, get_current_user, settings
+
+
+async def test_jwt():
+    data = {'sub': 'test@test.com'}
+    token = create_access_token(data)
+
+    result = decode(
+        token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+    )
+
+    assert result['sub'] == data['sub']
+    assert result['exp']
+
+
+async def test_jwt_invalid_token(client: AsyncClient):
+    response = await client.delete(
+        '/users/1', headers={'Authorization': 'Bearer token-invalido'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+async def test_get_current_user_missing_username(
+    session: AsyncSession, user: Account
+):
+    token = create_access_token(data={'bup': user.email})
+
+    with pytest.raises(HTTPException) as exc_info:
+        await get_current_user(session, token)
+
+    assert exc_info.value.status_code == HTTPStatus.UNAUTHORIZED
+
+
+async def test_get_current_user_without_user(session: AsyncSession):
+    user_fake = Account(
+        username='fake', email='fake@test.com', password='fake'
+    )
+    token = create_access_token(data={'sub': user_fake.email})
+
+    with pytest.raises(HTTPException) as exc_info:
+        await get_current_user(session, token)
+
+    assert exc_info.value.status_code == HTTPStatus.UNAUTHORIZED
+
+
+async def test_token_expired_after_time(client: AsyncClient, user: Account):
+    with freeze_time('2024-08-20 12:00:00'):
+        response = await client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password}
+        )
+
+        assert response.status_code == HTTPStatus.OK
+        token = response.json()['access_token']
+
+    with freeze_time('2024-08-20 12:31:00'):
+        response = await client.put(
+            f'/users/{user.id}',
+            headers={'Authorization': f'Bearer {token}'},
+            json={
+                'username': 'wrong',
+                'email': 'wrong@example.com',
+                'password': 'wrong',
+            }
+        )
+
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json() == {'detail': 'Could not validate credentials'}
