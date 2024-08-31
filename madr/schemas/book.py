7ce75b6ef5318355ae5ca2591fdb3b6edde2ed91
Carlos Eduardo <ducameneses16@gmail.com>
2024-08-30 23:11:36 -0300

    feat: Add validation the schemas the book

--- madr/schemas/book.py
+++ madr/schemas/book.py
@@ -1,31 +1,45 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
+
+MIN_YEAR = 900
+MAX_YEAR = 2099
 
 
 class BookSchema(BaseModel):
-    year: Annotated[str, Field(
-        description='year of release', examples=['1899'], max_length=4),
+    year: Annotated[
+        str,
+        Field(
+            description='year of release',
+            examples=['1899'],
+            max_length=4,
+            min_length=3,
+        ),
     ]
-    title: Annotated[str, Field(
-        description='title of book', examples=['Dom Casmurro'], max_length=50),
+    title: Annotated[
+        str,
+        Field(
+            description='title of book',
+            examples=['Dom Casmurro'],
+            max_length=50,
+        ),
     ]
     author_id: Annotated[int, Field(description='ID of the novelist')]
 
+    @field_validator('year')
+    @classmethod
+    def _year_validate(cls, year: str) -> str:
+        try:
+            year_int = int(year)
+            if MIN_YEAR <= year_int <= MAX_YEAR:
+                return year
 
-class BookPublic(BaseModel):
-    id: Annotated[int, Field(
-        description='ID of book')
-    ]
-    year: Annotated[str, Field(
-        description='year of release', examples=['1899'], max_length=4),
-    ]
-    title: Annotated[str, Field(
-        description='title of book', examples=['Dom Casmurro'], max_length=50),
-    ]
-    author_id: Annotated[int, Field(
-        description='ID of the novelist')
-    ]
+        except ValueError:
+            raise ValueError('Invalid year format')
+
+
+class BookPublic(BookSchema):
+    id: Annotated[int, Field(description='ID of book')]
 
 
 class BookList(BaseModel):
@@ -33,4 +47,22 @@ class BookList(BaseModel):
 
 
 class BookUpdate(BaseModel):
-    year: str
+    year: Annotated[
+        Optional[str],
+        Field(
+            description='year of release',
+            examples=['1899'],
+            max_length=4,
+            min_length=3,
+        ),
+    ]
+
+    @field_validator('year')
+    @classmethod
+    def _year_validate(cls, year: str) -> str:
+        try:
+            year_int = int(year)
+            if MIN_YEAR <= year_int <= MAX_YEAR:
+                return year
+        except ValueError:
+            raise ValueError('Invalid year format')
