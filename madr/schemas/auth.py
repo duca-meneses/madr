d0c0fce51920ff5aedb87cf654bf3d0e6de18ba0
Carlos Eduardo <ducameneses16@gmail.com>
2025-04-09 03:53:47 -0300

    feat: Add schema for password change

--- madr/schemas/auth.py
+++ madr/schemas/auth.py
@@ -10,3 +10,30 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: str | None = None
+
+
+class AuthPassword(BaseModel):
+    password: str = Field(
+        ...,
+        title='Password',
+        description='Current password to update',
+        min_length=6,
+        max_length=100,
+        examples=['string'],
+    )
+    new_password: str = Field(
+        ...,
+        title='New Password',
+        description='New password to update',
+        min_length=8,
+        max_length=100,
+        examples=['string@1'],
+    )
+    confirm_new_password: str = Field(
+        ...,
+        title='Confirm new Password',
+        description='Confirm new password',
+        min_length=8,
+        max_length=100,
+        examples=['string@1'],
+    )
