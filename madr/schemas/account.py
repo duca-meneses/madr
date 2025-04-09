1fc04188a6e2dc1d7cfc009bf0254ce0a81d3dad
Carlos Eduardo <ducameneses16@gmail.com>
2025-04-09 03:51:45 -0300

    fix: Add support for partial account updates

--- madr/schemas/account.py
+++ madr/schemas/account.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
@@ -9,7 +9,13 @@ class AccountSchema(BaseModel):
         Field(description='username of the user', max_length=25, min_length=3),
     ]
     email: Annotated[EmailStr, Field(description='email of the user')]
-    password: Annotated[str, Field(description='password of the user')]
+    password: Annotated[str, Field(
+        description='password of the user',
+        min_length=8,
+        max_length=100,
+        examples=['string12'],
+        )
+    ]
 
 
 class AccountPublic(BaseModel):
@@ -18,6 +24,15 @@ class AccountPublic(BaseModel):
     username: Annotated[str, Field(description='username of the user')]
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class AccountUpdateSchema(BaseModel):
+    username: Optional[Annotated[
+        str,
+        Field(description='username of the user', max_length=25, min_length=3),
+        ]
+    ]
+    email: Optional[Annotated[EmailStr, Field(description='email of the user')]]
 
 
 class AccountList(BaseModel):
